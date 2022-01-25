6db2d87907e73fc3a234fab67c3db30f55d788bc
adrivandevelde <vandevelde628@gmail.com>
2022-01-25 18:07:33 -0300

    Agregado de modelo, vista y url Temas

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -2,8 +2,8 @@
 
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request,'index.html', {} )
+    return redirect('post_app:post_list')
--- blog/post_app/views.py
+++ blog/post_app/views.py
@@ -1,14 +1,12 @@
 
 from email import contentmanager
-from pyexpat import model
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
-
-
-
-from post_app.models import Post, Usuarios
+from django.db.models import Q
+from post_app.models import Post, Usuarios, Temas
 
 # Create your views here.
 
@@ -23,7 +21,28 @@ class Post_listView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'post_list.html'
-    queryset = Post.objects.all()
+    
+class Post_sarch(ListView):
+    template_name =  'post_list.html' #se indica el template a utiliza 
+    #en este caso el queryset va a depender de lo que el snippets search nos mande en el atributo q
+    #queryset =  
+    
+
+    def get_queryset(self):#se hace la consulta        
+        filters = Q(nombre__icontains=self.query()) | Q(autor__icontains=self.query()) | Q(contenido__icontains=self.query())
+        return Post.objects.filter(filters)   
+
+    def query(self):#se obtiene el valor de q en el request
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs): #sobre esctibo el metodo para obtener el contexto de la peticion
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['posts'] = context['post_list']
+        
+        return context
+    
+    
     
 class Usuarios_listView(ListView):
     model= Usuarios
@@ -34,3 +53,13 @@ class Usuarios_addView(CreateView):
     model= Usuarios
     fields= ['username', 'nombre_usuario','apellido_usuario', 'email', 'profesion', 'edad']
     success_url =  reverse_lazy('post_app:usuarios_list')
+
+class Temas_listView(ListView):
+    model= Temas
+    context_object_name= 'temas'
+    template_name= 'temas_list.html'
+
+class Temas_addView(CreateView):
+    model= Temas
+    fields= ['categoria', 'descripcion']
+    success_url =  reverse_lazy('post_app:temas_list')
